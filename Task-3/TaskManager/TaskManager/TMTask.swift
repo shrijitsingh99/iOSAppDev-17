d90fbd4ab23a6c21844d87873612dba6593bf8fe
Shrijit Singh <shrijitsingh99@gmail.com>
2017-12-26 03:12:46 +0530

    Added ability to add tasks

---
+++ Task-3/TaskManager/TaskManager/TMTask.swift
@@ -0,0 +1,13 @@
+//
+//  TMTask.swift
+//  TaskManager
+//
+//  Created by Shrijit Singh on 19/12/17.
+//  Copyright © 2017 Shrijit Singh. All rights reserved.
+//
+
+import UIKit
+
+class TMTask: NSObject {
+
+}
